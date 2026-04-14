7a940c2f0b6a45b2a7ed3159f7a37fd6b15340e7
unknown <2510313@gmail.com>
2026-04-14 17:00:40 -0600

    fix: shadowJar y start command para Railway

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.ktor)
     id("org.jetbrains.kotlin.plugin.serialization") version "1.9.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "com"
@@ -15,36 +16,28 @@ kotlin {
     jvmToolchain(21)
 }
 
+tasks {
+    shadowJar {
+        archiveFileName.set("app.jar")
+        mergeServiceFiles()
+    }
+}
+
 dependencies {
     implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.netty)
     implementation(libs.logback.classic)
-    implementation(libs.ktor.server.core)
-    implementation(libs.ktor.server.config.yaml)
-    testImplementation(libs.ktor.server.test.host)
-    testImplementation(libs.kotlin.test.junit)
-
-}
-dependencies {
-    // Ktor Server Core y Netty
-    implementation(libs.ktor.server.core)
-    implementation(libs.ktor.server.netty)
     implementation(libs.ktor.server.config.yaml)
 
-    // Serialización para el Servidor (Esto es lo que envía los JSON a la App)
     implementation("io.ktor:ktor-server-content-negotiation:2.3.11")
     implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.11")
 
-    // Base de Datos (MySQL y Exposed)
     implementation("org.jetbrains.exposed:exposed-core:0.41.1")
     implementation("org.jetbrains.exposed:exposed-jdbc:0.41.1")
     implementation("mysql:mysql-connector-java:8.0.33")
     implementation("org.jetbrains.exposed:exposed-java-time:0.41.1")
     implementation("com.github.librepdf:openpdf:1.3.30")
-    // Logs
-    implementation(libs.logback.classic)
 
-    // Tests
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
 }
